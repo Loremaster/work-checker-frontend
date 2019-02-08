a86a8fcf3afc1c40ff6d65e0e30791eddcd9cddd
Serj Ler <gelvior@gmail.com>
2019-02-08 16:00:56 +0200

    Setup prettier. It also changes ts files now.
    Fixing all if it's warnings.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,7 @@
   <v-container>
     <v-layout text-xs-center wrap>
       <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
       </v-flex>
 
       <v-flex mb-4>
@@ -18,9 +13,7 @@
           {{ msg }}
           For help and collaboration with other Vuetify developers,
           <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
         </p>
       </v-flex>
 
@@ -28,13 +21,7 @@
         <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
 
         <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
+          <a v-for="(eco, i) in ecosystem" :key="i" :href="eco.href" class="subheading mx-3" target="_blank">
             {{ eco.text }}
           </a>
         </v-layout>
@@ -51,20 +38,18 @@ export default class HelloWorld extends Vue {
   ecosystem = [
     {
       text: "vuetify-loader",
-      href: "https://github.com/vuetifyjs/vuetify-loader"
+      href: "https://github.com/vuetifyjs/vuetify-loader",
     },
     {
       text: "github",
-      href: "https://github.com/vuetifyjs/vuetify"
+      href: "https://github.com/vuetifyjs/vuetify",
     },
     {
       text: "awesome-vuetify",
-      href: "https://github.com/vuetifyjs/awesome-vuetify"
-    }
+      href: "https://github.com/vuetifyjs/awesome-vuetify",
+    },
   ];
 
   @Prop() private msg!: string;
 }
 </script>
-
-<style></style>
