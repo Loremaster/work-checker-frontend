b1ef13fa80ccd40370f757c982536bd457de8ac8
Serj Ler <gelvior@gmail.com>
2019-02-15 15:51:26 +0200

    Storing jwt token in localstorage after successful login.

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -6,20 +6,36 @@
     </v-toolbar>
     <v-card-text>
       <v-form>
-        <v-text-field prepend-icon="person" name="email" label="Email" type="email" />
-        <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" />
+        <v-text-field prepend-icon="person" name="email" label="Email" type="email" v-model="email" />
+        <v-text-field
+          id="password"
+          prepend-icon="lock"
+          name="password"
+          label="Password"
+          type="password"
+          v-model="password"
+        />
       </v-form>
     </v-card-text>
     <v-card-actions>
       <v-spacer />
-      <v-btn color="primary">Login</v-btn>
+      <v-btn color="primary" @click="signin">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import api from "@/apis/authentication";
 
 @Component
-export default class Signin extends Vue {}
+export default class Signin extends Vue {
+  email: string = "";
+  password: string = "";
+
+  async signin() {
+    const response = await api.signin({ email: this.email, password: this.password });
+    localStorage.setItem("token", response.data.jwt);
+  }
+}
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import Vue from "vue";
-import "./plugins/vuetify";
+import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./plugins/vuetify";
 import "./registerServiceWorker";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL = `${process.env.VUE_APP_BACKEND_URL}/api/v1/`;
+
+const token = localStorage.getItem("token");
+
+if (token) {
+  axios.defaults.headers.common["Authorization"] = token;
+}
 
 new Vue({
   router,
