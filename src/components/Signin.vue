e7e6587c18329cb3386702d554fd6f28565018b4
Serj Ler <gelvior@gmail.com>
2019-03-05 21:15:08 +0300

    Adding tests for Signin component

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -7,19 +7,12 @@
     <v-card-text>
       <v-form>
         <v-text-field prepend-icon="person" name="email" label="Email" type="email" v-model="email" />
-        <v-text-field
-          id="password"
-          prepend-icon="lock"
-          name="password"
-          label="Password"
-          type="password"
-          v-model="password"
-        />
+        <v-text-field prepend-icon="lock" name="password" label="Password" type="password" v-model="password" />
       </v-form>
     </v-card-text>
     <v-card-actions>
       <v-spacer />
-      <v-btn color="primary" @click="login" :disabled="disabled">Login</v-btn>
+      <v-btn color="primary" class="submit" @click="login" :disabled="disabled">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
