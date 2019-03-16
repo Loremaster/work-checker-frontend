091f7632dbb3b3a37936b4a87e770ccc7435893e
Serj Ler <gelvior@gmail.com>
2019-03-16 20:14:31 +0300

    Setting html 5 history mode correctly:
    * Adding middleware to handle refresh correctly
    * Show 404 page when requested page doesn't exist

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+import NotFound from "./views/NotFound.vue";
 import Home from "./views/Home.vue";
 
 Vue.use(Router);
@@ -8,6 +9,7 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+    { path: "*", component: NotFound },
     {
       path: "/",
       name: "home",
