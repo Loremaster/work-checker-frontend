7074d898abf4d5a8bfb9e6f240d25a5ecc6578d0
Serj Ler <gelvior@gmail.com>
2019-03-31 17:46:35 +0300

    Hide "users" button when user is not an admin

--- src/getters/users.ts
+++ src/getters/users.ts
@@ -2,6 +2,9 @@ import { IStore, IUserList } from "@/interfaces";
 
 const users = (state: IStore): IUserList[] => state.users || [];
 
+const isAdmin = (state: IStore): boolean => state.user.role === "admin";
+
 export default {
   users,
+  isAdmin,
 };
